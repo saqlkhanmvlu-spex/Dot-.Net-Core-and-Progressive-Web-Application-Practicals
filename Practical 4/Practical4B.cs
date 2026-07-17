2e36b4774b612766d55a418b5d622666b234d13b
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-17 01:36:12 -0700

    Practical4B.cs

--- Practical 4/Practical4B.cs
+++ Practical 4/Practical4B.cs
@@ -1,1 +1,22 @@
+<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div> 
+
+        <asp:Label ID="Label1" runat="server" Text="Student Name"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="Name Cannot Be Empty"></asp:RequiredFieldValidator>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Enter Your Age"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server" TextMode="Number"></asp:TextBox>
+        <asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="TextBox2" ErrorMessage="Age Should Be Between 18 - 99" MaximumValue="99" MinimumValue="18" Type="Integer"></asp:RangeValidator>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Password"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        Confirm Password<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToCompare="TextBox3" ControlToValidate="TextBox4" ErrorMessage="Password Does Not Match"></asp:CompareValidator>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Submit" />
+
+   </div>
