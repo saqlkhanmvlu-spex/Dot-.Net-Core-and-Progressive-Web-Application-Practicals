424163ad81cb4456f03647bf66015fc422dbb629
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-17 01:37:30 -0700

    Practical4C.cs

--- Practical 4/Practical4C.cs
+++ Practical 4/Practical4C.cs
@@ -1,1 +1,15 @@
-
+<?xml version="1.0" encoding="utf-8" ?>
+<Advertisements>
+	<Ad>
+		<ImageUrl>Images/img1.png</ImageUrl>
+		<NavigateUrl>https://reze.com</NavigateUrl>
+		<AlternateText>img2</AlternateText>
+		<Height>100</Height>
+		<Width>100</Width>
+	</Ad>
+	<Ad>
+		<ImageUrl>Images/img2.png</ImageUrl>
+		<NavigateUrl>https://reze.com</NavigateUrl>
+		<AlternateText>Reze</AlternateText>
+	</Ad>
+</Advertisements>
