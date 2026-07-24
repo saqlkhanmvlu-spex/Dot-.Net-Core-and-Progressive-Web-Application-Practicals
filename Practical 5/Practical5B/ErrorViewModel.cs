78ed41a5b23f786b2f68c025b8266c65a2364159
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-23 23:43:59 -0700

    Create ErrorViewModel.cs

---
+++ Practical 5/Practical5B/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practical_5B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
