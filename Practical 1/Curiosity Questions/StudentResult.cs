ef91fae5569bbd69b715303d8aea9c827e7f98cf
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-29 12:23:00 +0530

    StudentResult.cs

---
+++ Practical 1/Curiosity Questions/StudentResult.cs
@@ -0,0 +1,14 @@
+namespace StudentResultMVC.Models
+{
+    public class StudentResult
+    {
+        public int Subject1 { get; set; }
+        public int Subject2 { get; set; }
+        public int Subject3 { get; set; }
+        public int Subject4 { get; set; }
+        public int Subject5 { get; set; }
+        public int Total { get; set; }
+        public double Percentage { get; set; }
+        public string Grade { get; set; }
+    }
+}
