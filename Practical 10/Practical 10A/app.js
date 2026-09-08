b2e123564ef8f3d5b30eaf9c03cf186208a1a634
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-09-08 08:48:31 +0530

    app.js

---
+++ Practical 10/Practical 10A/app.js
@@ -0,0 +1,17 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then(() => console.log('Service Worker Registered'));
+}
+
+function showNotification() {
+    Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+            navigator.serviceWorker.ready.then(registration => {
+                registration.showNotification('PWA Notification', {
+                    body: 'Push Notification Working Successfully!',
+                    icon: 'icons/icon1.png'
+                });
+            });
+        }
+    });
+}
