0065d828920d90ccf84bcdc2098ac502e7424768
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-09-08 08:45:23 +0530

    Student.cs

---
+++ Practical 9/Practical 9A/Student.cs
@@ -0,0 +1,12 @@
+namespace Practical_9A.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+    }
+}
+
+
